67411e5df1cf0449a3f91e7d720c4864f57b8ca0
Tara Z. Manicsic <tara@modulus.io>
2016-06-07 23:56:02 -0400

    Add registry placeholder bits and styling.
    Created placeholder items in Registry container.
    Added styles and intro text for registry.

--- src/containers/Registry.js
+++ src/containers/Registry.js
@@ -2,12 +2,169 @@ import React, {createClass} from 'react'
 
 import DefaultLayout from '../layouts/Default'
 
-let Registry = createClass({
+let containerClasses = 'registry-container container'
+
+const Registry = createClass({
   render () {
     return (
       <DefaultLayout classes='page-home'>
-        <div className='registry-items'>
-          <h1>COMING SOON!</h1>
+        <div className={containerClasses}>
+          <div className='registry-intro'>
+            <p>
+              Some of the best adventures we have had were while hiking and
+              exploring the outdoors, especially National Parks. One park we
+              have always dreamed about traveling to is
+              <a href='http://tinyurl.com/hkpknsu'> Torres del Paine National Park. </a>
+              Torres del Paine in located in the southern Chilean Patagonian &amp;
+              known for the beautiful mountain, glacial lakes, flowering meadows
+              and active wildlife.
+            </p>
+            <p>
+              Being together for 8 years, 3 apartments and now 1 house (yay!)
+              , we have LOTS of stuff! A trip to Torres del Paine would be an
+              adventure of a lifetime. So in lieu of a normal registry we would
+              love for you to help us plan (& pay) for parts of our dream
+              honeymoon.
+            </p>
+            <h3>Thank you, thank you, thank you! xoxo Matthew & Tara</h3>
+          </div>
+          <div className='registry-items'>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+            <div className='registry-item'>
+              <div className='item-image'></div>
+              <p className='item-description'>
+                A Night at Tierra Patagonia Hotel & Spa
+              </p>
+              <p className='item-price'>
+                $10
+              </p>
+            </div>
+          </div>
         </div>
       </DefaultLayout>
     )
